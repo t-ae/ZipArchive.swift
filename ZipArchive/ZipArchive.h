fe5440090eb37ee11ffb321bbed8bb5633cf9992
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2016-05-08 02:04:55 +0900

    Add Xcode project

---
+++ ZipArchive/ZipArchive.h
@@ -0,0 +1,19 @@
+//
+//  ZipArchive.h
+//  ZipArchive
+//
+//  Created by Yasuhiro Hatta on 2016/05/08.
+//  Copyright © 2016年 yaslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZipArchive.
+FOUNDATION_EXPORT double ZipArchiveVersionNumber;
+
+//! Project version string for ZipArchive.
+FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/PublicHeader.h>
+
+
